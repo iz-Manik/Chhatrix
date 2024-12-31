84442946015bcd45b3301cd3993a2ee4dcd06ad3
Manik <manikchauhan029@gmail.com>
2025-01-01 03:57:20 +0530

    correcting frontend

--- client/src/appRoutes.js
+++ client/src/appRoutes.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import homeScreen from "./screens/home/homeScreen";
 import loginScreen from "./screens/auth/login/loginScreen";
 import signupScreen from "./screens/auth/signup/signupScreen";
 
-export default class Route extends Component {
+export default class AppRoutes extends Component {
   render() {
     return (
       <Router>
--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import Routes from "./routes";
+import AppRoutes from "./appRoutes";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
